e33c1999254045f68e40a7b20358a11c55515665
Erik Rainey <erik.rainey@gmail.com>
2022-10-23 19:12:52 -0500

    Adding Conan Configurations

---
+++ projects/pyhobbies/tests/test_scene.py
@@ -0,0 +1,10 @@
+import pytest
+
+#def test_scene_simple_sphere() -> None:
+#    from pyhobbies.raytrace import scene, sphere, point
+#    scene = scene()
+#    scene.from(point(7,8,9))
+#    scene.at(point(6,7,8))
+#    scene.add(sphere(point(1,2,3), 5))
+#    scene.render()
+#    scene.save("file.tga")

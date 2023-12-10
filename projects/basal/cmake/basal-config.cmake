c91cb430280ec469ac45b67c176981f2e7b0f8e3
Erik Rainey <erik.rainey@gmail.com>
2023-12-10 10:55:47 -0600

    WIP: Condensing into a Single CMake build
    Currently this doesn't work as the export() function doesn't work to allow
    further subprojects to bring the dependencies in the same way that an
    install export does.

--- projects/basal/cmake/basal-config.cmake
+++ projects/basal/cmake/basal-config.cmake
@@ -1,2 +1,2 @@
 # This file is meant to be installed then "found" via find_package()
-include("${CMAKE_CURRENT_LIST_DIR}/BasalTargets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/basalTargets.cmake")

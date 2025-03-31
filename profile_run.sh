9ac0d4d7f1f3120da62e6c29a0ecebb1f4453cdd
Erik Rainey <erik.rainey@gmail.com>
2025-03-30 19:32:41 -0500

    Updating gperftool version
    Since I'm primarily using LLVM now (as native clang no
    longer support OpenMP), the version of profiling tools
    are mismatched between LLVM and native clang.
    
    I've commented out the pprof tool (they don't seem to work) in cmake
    but the script does work oddly.
    
    The `llvm-profdata` tool is version 8 but LLVM 19.1.7_1 is version 10.

--- profile_run.sh
+++ profile_run.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
 export CPUPROFILE_FREQUENCY=1000
-export DYLD_INSERT_LIBRARIES=/opt/homebrew/Cellar/gperftools/2.15/lib/libprofiler.dylib
-_PATH=`pwd`/build/native-llvm-19/projects/raytrace
+export DYLD_INSERT_LIBRARIES=/opt/homebrew/Cellar/gperftools/2.16/lib/libprofiler.dylib
+_PATH=`pwd`/build/native-llvm-19-profiled/projects/raytrace
 export DYLD_LIBRARY_PATH=./install/lib:${_PATH}
-_PRGM=demo_curses
+_PRGM=demo_sdl2
 _FILE=${_PRGM}-perf.pdf
 export CPUPROFILE=${_PRGM}.prof
 
-(cd testing && ${_PATH}/${_PRGM} -m world_example -d 1)
+(cd testing && ${_PATH}/${_PRGM} -m world_example)
 (cd testing && pprof --pdf ${_PATH}/${_PRGM} ${CPUPROFILE} > ${_FILE})

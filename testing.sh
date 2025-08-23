740646e67f17893de307449ed72541f97f433fed
Erik Rainey <erik.rainey@gmail.com>
2025-08-23 12:46:34 -0500

    Fixes to XMMT and Unit tests as well as testing script

--- testing.sh
+++ testing.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+if [[ -z ${PRJ_ROOT} ]]; then
+    PRJ_ROOT=`pwd`/build/homebrew-llvm/projects
+fi
 unset PRJ_PATH
-for prj in `ls -1 build/homebrew-llvm/projects`; do
+for prj in `ls -1 ${PRJ_ROOT}`; do
     #echo "${prj}"
-    PRJ_PATH=${PRJ_PATH}:`pwd`/build/homebrew-llvm/projects/${prj}
+    PRJ_PATH=${PRJ_PATH}:${PRJ_ROOT}/${prj}
 done
 
-export DYLD_LIBRARY_PATH=${PRJ_PATH}
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${PRJ_PATH}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${PRJ_PATH}
 export PATH=${PATH}:${PRJ_PATH}
 echo "============="
 echo "DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}"
 echo "============="
+echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
+echo "============="
 echo "PATH=${PATH}"
-(cd testing && $*)
+(cd testing && ../$*)
